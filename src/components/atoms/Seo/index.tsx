7ae41ea1581da06a14df8776e129b9d6b32471e3
greatSumini <greatSumini@gmail.com>
2022-03-22 18:32:42 +0900

    feat(components): :sparkles: add atoms/Seo

---
+++ src/components/atoms/Seo/index.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import config from "~/admin.config.json";
+
+export type SeoProps = {
+  title?: string;
+  type?: string;
+};
+
+const SITE_NAME = config.name;
+
+export function Seo(props: SeoProps) {
+  const router = useRouter();
+
+  const meta = {
+    type: "website",
+    ...props,
+  };
+
+  const title = props.title ? `${props.title} | ${SITE_NAME}` : SITE_NAME;
+
+  return (
+    <Head>
+      <title>{title}</title>
+
+      <meta property="og:type" content={meta.type} />
+      <meta property="og:site_name" content={SITE_NAME} />
+      <meta property="og:locale" content="ko_KR" />
+
+      <meta name="robots" content="noindex" />
+    </Head>
+  );
+}
