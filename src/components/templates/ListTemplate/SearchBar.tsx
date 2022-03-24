5e325f52edff75ffec7dd94bd7d68f356bbe09e5
greatSumini <greatSumini@gmail.com>
2022-03-24 19:24:11 +0900

    feat(components): :sparkles: add ListTemplate.SearchBar

---
+++ src/components/templates/ListTemplate/SearchBar.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+import { SearchIcon } from "@/icons";
+
+import styles from "./SearchBar.module.scss";
+
+export default function SearchBar({ field }: { field: string }) {
+  const [text, setText] = useState("");
+
+  const queryName = `${field}Like`;
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const query = urlParams.get(queryName);
+
+    if (query != null) {
+      setText(query);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const search = () => {
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set(queryName, text);
+    location.search = urlParams.toString();
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={search}>
+        <SearchIcon />
+      </button>
+    </div>
+  );
+}
