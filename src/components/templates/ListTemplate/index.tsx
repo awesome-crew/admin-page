b5c053cdfb0cad3934ef09f50d1d153aa11767d0
greatSumini <greatSumini@gmail.com>
2022-03-24 21:26:29 +0900

    feat(components): :sparkles: add templates/ListTemplate

---
+++ src/components/templates/ListTemplate/index.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+
+import { Button } from "@/components/atoms";
+import SearchBar from "./SearchBar";
+import Buttons, { ListButtonsProps } from "./Buttons";
+import Table, { ListTableProps } from "./Table";
+
+import { useListData } from "./useListData";
+
+import styles from "./index.module.scss";
+
+import config from "~/admin.config.json";
+
+export type ListTemplateProps<Model> = {
+  name: string;
+} & Pick<ListButtonsProps, "buttons"> &
+  Pick<ListTableProps<Model>, "fields">;
+
+export function ListTemplate<Model>({
+  name,
+  buttons,
+  fields,
+}: ListTemplateProps<Model>) {
+  const section = config.sections.find((section) =>
+    section.models.find((model) => model.name === name)
+  );
+  const model = section.models.find((model) => model.name === name);
+
+  const data = useListData<Model>(name);
+
+  return (
+    <>
+      <span className={styles.breadcrumb}>
+        {section.name} / {model.label}
+      </span>
+      <h1 className={styles.title}>List</h1>
+      <div className={styles.actions}>
+        {model.searchField != null && <SearchBar field={model.searchField} />}
+        <Buttons buttons={buttons}>
+          {model.create === true && (
+            <Link href={`/${name}s/create`}>
+              <a>
+                <Button>Create new</Button>
+              </a>
+            </Link>
+          )}
+        </Buttons>
+      </div>
+      <Table
+        modelName={model.name}
+        data={data}
+        fields={fields}
+        delete={model.delete}
+        detail={model.detail}
+      />
+    </>
+  );
+}
