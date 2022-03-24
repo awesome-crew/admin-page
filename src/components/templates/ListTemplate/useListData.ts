f83cdc634dadae8384709539592f7f6324c344d7
greatSumini <greatSumini@gmail.com>
2022-03-24 20:30:42 +0900

    enhance(components): add default pagination to ListTemplate.useListData

--- src/components/templates/ListTemplate/useListData.ts
+++ src/components/templates/ListTemplate/useListData.ts
@@ -2,11 +2,21 @@ import { useEffect, useState } from "react";
 
 import { api } from "@/services";
 
+import { PAGE_SIZE } from "./constants";
+
 export const useListData = <Model>(modelName: string) => {
   const [data, setData] = useState<Model[]>([]);
 
   const fetch = async () => {
-    const res = await api.get<Model[]>(`/${modelName}s${location.search}`);
+    const params = new URLSearchParams(location.search);
+    if (!params.get("offset")) {
+      params.set("offset", "0");
+    }
+    if (!params.get("limit")) {
+      params.set("limit", PAGE_SIZE.toString());
+    }
+
+    const res = await api.get<Model[]>(`/${modelName}s?${params.toString()}`);
     setData(res);
   };
 
