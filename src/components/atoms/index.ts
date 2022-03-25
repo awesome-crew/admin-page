18b96d2e2063d4d9850e906425d40666d9ef88e4
ybh1760 <ybh1760@gmail.com>
2022-03-25 16:04:15 +0900

    feat: :sparkles: implement boolean filter feature

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,2 +1,3 @@
 export * from "./Button";
+export * from "./CheckboxInput";
 export * from "./Seo";
--- src/components/templates/ListTemplate/Filter.tsx
+++ src/components/templates/ListTemplate/Filter.tsx
@@ -1,3 +1,7 @@
+import { ChangeEvent, useEffect, useState } from "react";
+
+import { CheckboxInput } from "@/components/atoms";
+
 import styles from "./Filter.module.scss";
 
 export type FilterType = "boolean" | "string" | "number" | "enum";
@@ -18,10 +22,61 @@ export type ListFilterProps = {
   >;
 };
 
-export default function ListFilter(props: ListFilterProps) {
+export default function ListFilter({ filters }: ListFilterProps) {
+  const [filterValues, setFilterValues] =
+    useState<{ [name in string]?: boolean | string | number }>();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const filterValues = filters.reduce((acc, filter) => {
+      switch (filter.type) {
+        case "boolean":
+          return {
+            ...acc,
+            [filter.name]: urlParams.get(filter.name) === "true",
+          };
+      }
+
+      return acc;
+    }, {});
+
+    setFilterValues(filterValues);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const urlParams = new URLSearchParams(location.search);
+    const filter = filters.find(({ name }) => name === e.target.name);
+    switch (filter.type) {
+      case "boolean":
+        e.target.checked
+          ? urlParams.set(filter.name, "true")
+          : urlParams.delete(filter.name);
+    }
+
+    urlParams.set("offset", "0");
+    urlParams.set("limit", "20");
+
+    location.search = urlParams.toString();
+  };
+
   return (
     <section className={styles.wrapper}>
       <h2 className={styles.title}>Filter</h2>
+      {filterValues &&
+        filters.map((filter) => {
+          switch (filter.type) {
+            case "boolean":
+              return (
+                <CheckboxInput
+                  name={filter.name}
+                  value={filterValues[filter.name] as boolean}
+                  onChange={handleChange}
+                />
+              );
+          }
+        })}
     </section>
   );
 }
