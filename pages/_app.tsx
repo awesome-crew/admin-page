eb8903d88bacd0cfe17f78565e7c0cd3318352dd
greatSumini <greatSumini@gmail.com>
2022-03-28 15:17:37 +0900

    feat(): set recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 import { GeistProvider, CssBaseline } from "@geist-ui/core";
 
 import "inter-ui/inter.css";
@@ -6,10 +7,12 @@ import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <GeistProvider>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </GeistProvider>
+    <RecoilRoot>
+      <GeistProvider>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </GeistProvider>
+    </RecoilRoot>
   );
 }
 
