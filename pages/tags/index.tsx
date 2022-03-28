0f64f8a373e4f39792a151a15349d91685c19b35
greatSumini <greatSumini@gmail.com>
2022-03-29 00:38:35 +0900

    temp(pages): add sampe Tags page

---
+++ pages/tags/index.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+
+import { MainLayout, ListTemplate } from "@/components/templates";
+
+import { Tag } from "@/models";
+
+export default function Tags() {
+  return (
+    <MainLayout>
+      <ListTemplate<Tag>
+        name="tag"
+        fields={[
+          "id",
+          { label: "이름", name: "name" },
+          { label: "Default 여부", name: "isDefault" },
+          { label: "가입시 추천 여부", name: "isBase" },
+          { label: "포트폴리오 수", name: "userCount" },
+          { label: "프로젝트 수", name: "projectCount" },
+        ]}
+      />
+    </MainLayout>
+  );
+}
