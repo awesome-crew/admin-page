5b9030ed82a7b825130b3d95365d0403aa81fac8
greatSumini <greatSumini@gmail.com>
2022-03-29 10:51:30 +0900

    temp(pages): add sample filters to Tags page

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -18,6 +18,10 @@ export default function Tags() {
           { label: "포트폴리오 수", name: "userCount" },
           { label: "프로젝트 수", name: "projectCount" },
         ]}
+        filters={[
+          { label: "Default 여부", name: "isDefault", type: "boolean" },
+          { label: "가입시 추천 여부", name: "isBase", type: "boolean" },
+        ]}
       />
     </MainLayout>
   );
