33bc3d18d5dcdeb1d95d21db14d17d7a17e825ce
양병훈 <ybh1760@gmail.com>
2022-03-29 15:17:22 +0900

    Enhance/image url field (#6)
    * chore(icons): :sparkles: add PlusIcon, SpinIcon
    
    * enhance(molecules) : update ImageUrl Field design

--- src/components/molecules/Field/ImageUrl.tsx
+++ src/components/molecules/Field/ImageUrl.tsx
@@ -4,8 +4,10 @@ import { ChangeEvent, useState } from "react";
 
 import { useForm } from "@/hooks";
 import { CommonService } from "@/services";
+import { PlusIcon, SpinIcon } from "@/icons";
 
 import { BaseField, BaseFieldProps } from "./Base";
+import styles from "./ImageUrl.module.scss";
 
 export function ImageUrlField(props: Omit<BaseFieldProps<string>, "children">) {
   const { name, value, editable = true } = props;
@@ -13,6 +15,7 @@ export function ImageUrlField(props: Omit<BaseFieldProps<string>, "children">) {
   const [isUploading, setUploading] = useState(false);
 
   const { form, update } = useForm();
+  const currentValue = form[name] ?? value;
 
   const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -38,8 +41,23 @@ export function ImageUrlField(props: Omit<BaseFieldProps<string>, "children">) {
 
   return (
     <BaseField {...props}>
-      <img src={form[name] ?? value} />
-      {editable && <input type="file" onChange={handleChange} />}
+      {!editable && (
+        <div className={styles.image_wrapper}>
+          <img src={currentValue} />
+        </div>
+      )}
+      {editable && (
+        <label className={styles.wrapper}>
+          {currentValue && <img src={currentValue} />}
+          {!currentValue && (
+            <>
+              {isUploading ? <SpinIcon /> : <PlusIcon />}
+              <p className={styles.upload}>Upload</p>
+            </>
+          )}
+          <input type="file" onChange={handleChange} hidden />
+        </label>
+      )}
     </BaseField>
   );
 }
