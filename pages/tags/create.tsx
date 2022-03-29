15ddfa69acc40887da9acc1e58782e383f96a312
greatSumini <greatSumini@gmail.com>
2022-03-29 10:46:42 +0900

    temp(pages): add sampe TagCreate page

---
+++ pages/tags/create.tsx
@@ -0,0 +1,17 @@
+import { MainLayout } from "@/components/templates";
+import { CreateTemplate } from "@/components/templates/CreateTemplate";
+
+export default function TagCreate() {
+  return (
+    <MainLayout>
+      <CreateTemplate
+        name="tag"
+        fields={[
+          { label: "이름", name: "name", type: "string" },
+          { label: "Default 여부", name: "isDefault", type: "boolean" },
+          { label: "가입시 추천 여부", name: "isBase", type: "boolean" },
+        ]}
+      />
+    </MainLayout>
+  );
+}
