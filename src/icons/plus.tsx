d70dd3f43266f9bdd17779a9ba693c4981198a97
ybh1760 <ybh1760@gmail.com>
2022-03-30 14:41:24 +0900

    chore(icons): add default style props to PlusIcon

--- src/icons/plus.tsx
+++ src/icons/plus.tsx
@@ -2,7 +2,12 @@ import { IconProps } from "./icon";
 
 export function PlusIcon(props: IconProps) {
   return (
-    <svg viewBox="64 64 896 896" width="14" height="14" fill="black" {...props}>
+    <svg
+      viewBox="64 64 896 896"
+      fill="black"
+      style={{ width: 14, height: 14 }}
+      {...props}
+    >
       <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8z"></path>
       <path d="M176 474h672q8 0 8 8v60q0 8-8 8H176q-8 0-8-8v-60q0-8 8-8z"></path>
     </svg>
